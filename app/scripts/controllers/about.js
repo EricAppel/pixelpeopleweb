1b0b33b9a8c74b8ff241a6683b296db7cf328ea6
Eric Appel <eric.appel@gmail.com>
2014-07-27 21:19:17 -0700

    initial commit of angular app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pixelPeopleWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pixelPeopleWebApp
+ */
+angular.module('pixelPeopleWebApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pixelPeopleWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pixelPeopleWebApp
+ */
+angular.module('pixelPeopleWebApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
